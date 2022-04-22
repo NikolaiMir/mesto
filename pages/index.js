ffb7bf47204cd76e75564ac5060dae78d435270e
Nicolay Mironyuk <mironyuk.nick@gmail.com>
2022-04-22 22:40:13 +0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 let editBtn = document.querySelector(".profile__button");
 let popup = document.querySelector(".popup");
 let closeBtn = document.querySelector(".popup__btn-close");
-let name = document.querySelector(".profile__name").textContent;
-let about = document.querySelector(".profile__about").textContent;
+let name = document.querySelector(".profile__name");
+let about = document.querySelector(".profile__about");
 let nameInput = document.querySelector("#name");
 let jobInput = document.querySelector("#job");
+let formElement = document.querySelector(".popup__container");
 
 function openModel() {
   popup.classList.add("popup_opened");
-  nameInput.value = name;
-  jobInput.value = about;
+  nameInput.value = name.textContent;
+  jobInput.value = about.textContent;
 }
 
 function closeModel() {
